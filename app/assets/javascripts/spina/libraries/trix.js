59da0210aee64288dc6aeb0a42c6b4d506a50da6
Adam Haris <iamadamharis@gmail.com>
2022-08-30 14:29:18 +0200

    Update trix.js

--- app/assets/javascripts/spina/libraries/trix.js
+++ app/assets/javascripts/spina/libraries/trix.js
@@ -7,3 +7,9 @@ Trix.config.blockAttributes.heading3 = {
   tagName: "h3", terminal: true, breakOnReturn: true, group: false }
 Trix.config.blockAttributes.heading4 = {
   tagName: "h4", terminal: true, breakOnReturn: true, group: false }
+Trix.config.blockAttributes.heading5 = {
+  tagName: "h5", terminal: true, breakOnReturn: true, group: false }
+Trix.config.blockAttributes.heading6 = {
+  tagName: "h6", terminal: true, breakOnReturn: true, group: false }
+Trix.config.blockAttributes.heading7 = {
+  tagName: "h7", terminal: true, breakOnReturn: true, group: false }
